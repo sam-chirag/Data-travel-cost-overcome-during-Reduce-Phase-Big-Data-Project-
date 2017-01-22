7fcc343776d3058f0f2b94dc994b720b4d4a2794
sam-chirag <pandeyshekhar20152@gmail.com>
2017-01-22 11:21:13 +0530

    Big Data and Hadoop Project

---
+++ Data-travel-cost-overcome-during-Reduce-Phase-Big-Data-Project-/PROJ5298/src/traffic/calculation5.java
@@ -0,0 +1,82 @@
+package traffic;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class calculation5 {
+	public static long exectime;
+	static long startTime = System.currentTimeMillis();
+	public static class Map extends MapReduceBase implements
+			Mapper<LongWritable, Text, Text, IntWritable> {
+		private final static IntWritable one = new IntWritable(1);
+		private Text word = new Text();
+
+		public void map(LongWritable key, Text value,
+				OutputCollector<Text, IntWritable> output, Reporter reporter)
+				throws IOException {
+			String line = value.toString();
+			System.out.println("Line == : "+line);
+			StringTokenizer tokenizer = new StringTokenizer(line);
+			while (tokenizer.hasMoreTokens()) {
+				word.set(tokenizer.nextToken());
+				output.collect(word, one);
+			}
+		}
+	}
+
+	public static class Reduce extends MapReduceBase implements
+			Reducer<Text, IntWritable, Text, IntWritable> {
+		public void reduce(Text key, Iterator<IntWritable> values,
+				OutputCollector<Text, IntWritable> output, Reporter reporter)
+				throws IOException {
+			int sum = 0;
+			while (values.hasNext()) {
+				sum += values.next().get();
+			}
+			output.collect(key, new IntWritable(sum));
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		JobConf conf = new JobConf(calculation5.class);
+		conf.setJobName("wordcount");
+
+		conf.setOutputKeyClass(Text.class);
+		conf.setOutputValueClass(IntWritable.class);
+
+		conf.setMapperClass(Map.class);
+		/*conf.setCombinerClass(Reduce.class);
+		conf.setReducerClass(Reduce.class);*/
+
+		conf.setInputFormat(TextInputFormat.class);
+		conf.setOutputFormat(TextOutputFormat.class);
+
+		FileInputFormat.setInputPaths(conf, new Path("/hadoop/mapred/system/JOB5.txt"));		
+		
+		FileOutputFormat.setOutputPath(conf, new Path("/hadoop/mapred/system/outjob5.txt"));
+		FileSystem fs=FileSystem.get(conf);
+		if(fs.exists(new Path("/hadoop/mapred/system/JOB5.txt")))
+			fs.delete(new Path("/hadoop/mapred/system/outjob5.txt"));
+		
+		
+		JobClient.runJob(conf);
+		 exectime=System.currentTimeMillis() - startTime;
+		System.out.println("Completed in "+(exectime));
+		/*BufferedWriter bw=new BufferedWriter(new FileWriter("./DTG.txt",true));
+		bw.write(String.valueOf(exectime));
+		bw.newLine();
+		bw.close();*/
+		/*System.out.println("==============Cluster Statuc =================");
+		System.out.println(new JobClient().getClusterStatus());*/
+		
+	}
+}
---
+++ Data-travel-cost-overcome-during-Reduce-Phase-Big-Data-Project-/PROJ5298/src/traffic/timeslots.java
@@ -0,0 +1,140 @@
+package traffic;
+
+
+
+
+import java.awt.Color;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.xy.XYDataset;
+import org.jfree.data.xy.XYSeries;
+import org.jfree.data.xy.XYSeriesCollection;
+import org.jfree.ui.ApplicationFrame;
+import org.jfree.ui.RefineryUtilities;
+
+public class timeslots extends ApplicationFrame {
+Connection con;
+       Statement st;
+       ResultSet rs;
+       public static ArrayList<Long>cost=new ArrayList<Long>();
+       public static ArrayList<Double>ar=new ArrayList<Double>();
+    public timeslots(final String title) {
+
+        super(title);
+
+        final XYDataset dataset = createDataset();
+        final JFreeChart chart = createChart(dataset);
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new java.awt.Dimension(800, 570));
+        setContentPane(chartPanel);
+
+    }
+  
+ 
+    private XYDataset createDataset() {
+       
+        
+        try{
+            Class.forName("com.mysql.jdbc.Driver");
+            con=(Connection)DriverManager.getConnection("jdbc:mysql://localhost:3306/cost","root","");
+            st=(Statement)con.createStatement();
+            String Query="select * from powcost";
+            rs=st.executeQuery(Query);
+            int x=0;
+          while(rs.next()){
+           
+                cost.add(rs.getLong(3));
+                ar.add(rs.getDouble(4));
+               System.out.println(rs.getLong(3));
+               System.out.println(rs.getDouble(4));
+                
+          x++;
+              }   
+        }catch(Exception e){System.out.println(e);}
+
+    
+        final XYSeries series1 = new XYSeries("Joint Optimization");
+        series1.add(0, 0.5);
+        series1.add(1, 1.5);
+        series1.add(2, 1.8);
+        series1.add(3, 2.0);
+        series1.add(4, 1.5);
+       // series1.add(60.0, 3.0);
+      // series1.add(70.0, 3.5);
+//        series1.add(80.0, 3.5);
+       
+
+        final XYSeries series2 = new XYSeries("Non Joint Optimization");
+        series2.add(0, 1);
+        series2.add(1, 1.5);
+        series2.add(2, 2.0);
+        series2.add(3, 2.5);
+        series2.add(4, 3.2);
+      //  series2.add(65.0, 4565);
+       // series2.add(75.0, 7.5);
+//        series2.add(85.0, 7);
+        final XYSeriesCollection dataset = new XYSeriesCollection();
+        dataset.addSeries(series1);
+        dataset.addSeries(series2);
+          
+        return dataset;
+        
+    }
+  
+    private JFreeChart createChart(final XYDataset dataset) {
+        
+        // create the chart...
+        final JFreeChart chart = ChartFactory.createXYLineChart(
+            "",      // chart title
+            "Time Slots",                      // x axis label
+            "Network Traffic",                      // y axis label
+            dataset,                  // data
+            PlotOrientation.VERTICAL,
+            true,                     // include legend
+            true,                     // tooltips
+            false                     // urls
+        );
+
+
+        chart.setBackgroundPaint(Color.white);
+
+        final XYPlot plot = chart.getXYPlot();
+        plot.setBackgroundPaint(Color.lightGray);
+         plot.setDomainGridlinePaint(Color.white);
+        plot.setRangeGridlinePaint(Color.white);
+        
+//        final XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
+//        renderer.setSeriesLinesVisible(0, false);
+//        renderer.setSeriesShapesVisible(1, false);
+//        plot.setRenderer(renderer);
+
+          final NumberAxis rangeAxis = (NumberAxis) plot.getRangeAxis();
+        rangeAxis.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
+
+        return chart;
+        
+    }
+
+  
+    public static void main(final String[] args) {
+
+
+        final timeslots demo = new timeslots("");
+        demo.pack();
+        RefineryUtilities.centerFrameOnScreen(demo);
+        demo.setVisible(true);
+
+    }
+
+}
